5ca28235554ff13036311239fa9cb5774e7a143c
Alagappan-a <105985376+Alagappan-a@users.noreply.github.com>
2022-05-29 22:08:26 +0530

    Add files via upload

---
+++ gradingStudents.java
@@ -0,0 +1,31 @@
+class gradingStudents {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+      List<Integer> result = new ArrayList<>();
+for (int i=0;i<grades.size();i++) {
+    int roundOff = grades.get(i)/5;
+    if(grades.get(i)>37){
+        if((((roundOff+1)*5)-grades.get(i))<3){
+            roundOff=((roundOff+1)*5);
+            result.add(roundOff);
+        }
+        else{
+         result.add(grades.get(i));
+    }
+    }
+    else{
+         result.add(grades.get(i));
+    }
+
+}
+return result;
+    }
+}
